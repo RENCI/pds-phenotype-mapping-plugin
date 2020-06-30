6326ad635e967df9a487b5b81ae9a0b709db8bec
hyi <hongyi@renci.org>
2020-06-30 15:28:13 -0400

    updated code to comply with the updated api spec

--- api/__init__.py
+++ api/__init__.py
@@ -1,48 +1,52 @@
 import pdsphenotypemapping.dispatcher
-import os
+
 
 def mappingClinicalFromData(patient_id, timestamp, body):
     if "variableTypes" not in body:
-        body["variableTypes"] = config["supportedPatientVariables"]
+        body["variableTypes"] = config['settingsDefaults']["patientVariables"]
         
     return pdsphenotypemapping.dispatcher.lookupClinicalsFromData(patient_id, timestamp, body)
 
+
 config = {
     "title": "smarthealthit.org variable mapper",
+    'piid': "pdspi-mapper-example",
     "pluginType": "m",
-    "pluginTypeTitle": "Mapping",
-    "pluginSelectors": [],
-    "supportedPatientVariables": [
-        {
-            "id": i,
-            "title": t,
-            "legalValues": lv
-        } for i,t,lv in [
-            ("LOINC:2160-0", "Serum creatinine", {"type": "number"}),
-            ("LOINC:82810-3", "Pregnancy", {"type": "boolean"}),
-            ("HP:0001892", "Bleeding", {"type": "boolean"}),
-            ("HP:0000077", "Kidney dysfunction", {"type": "boolean"}),
-            ("LOINC:45701-0", "Fever", {"type": "boolean"}),
-            ("LOINC:LP212175-6", "Date of fever onset", {"type": "string"}),
-            ("LOINC:64145-6", "Cough", {"type": "boolean"}),
-            ("LOINC:85932-2", "Date of cough onset", {"type": "string"}),
-            ("LOINC:54564-0", "Shortness of breath", {"type": "boolean"}),
-            ("LOINC:LP172921-1", "Cardiovascular disease", {"type": "boolean"}),
-            ("LOINC:54542-6", "Pulmonary disease", {"type": "boolean"}),
-            ("LOINC:LP128504-0", "Autoimmune disease", {"type": "boolean"}),
-            ("LOINC:LP21258-6", "Oxygen saturation", {"type": "number"}),
-            ("LOINC:30525-0", "Age", {"type": "integer"}),
-            ("LOINC:54134-2", "Race", {"type": "string"}),
-            ("LOINC:54120-1", "Ethnicity", {"type": "string"}),
-            ("LOINC:21840-4", "Sex", {"type": "string"}),
-            ("LOINC:8302-2", "Height", {"type": "number"}),
-            ("LOINC:29463-7", "Weight", {"type": "number"}),
-            ("LOINC:56799-0", "Address", {"type": "string"}),
-            ("LOINC:39156-5", "BMI", {"type": "number"})
+    "settingsDefaults": {
+        "pluginSelectors": [],
+        "patientVariables": [
+            {
+                "id": i,
+                "title": t,
+                "legalValues": lv
+            } for i,t,lv in [
+                ("LOINC:2160-0", "Serum creatinine", {"type": "number"}),
+                ("LOINC:82810-3", "Pregnancy", {"type": "boolean"}),
+                ("HP:0001892", "Bleeding", {"type": "boolean"}),
+                ("HP:0000077", "Kidney dysfunction", {"type": "boolean"}),
+                ("LOINC:45701-0", "Fever", {"type": "boolean"}),
+                ("LOINC:LP212175-6", "Date of fever onset", {"type": "string"}),
+                ("LOINC:64145-6", "Cough", {"type": "boolean"}),
+                ("LOINC:85932-2", "Date of cough onset", {"type": "string"}),
+                ("LOINC:54564-0", "Shortness of breath", {"type": "boolean"}),
+                ("LOINC:LP172921-1", "Cardiovascular disease", {"type": "boolean"}),
+                ("LOINC:54542-6", "Pulmonary disease", {"type": "boolean"}),
+                ("LOINC:LP128504-0", "Autoimmune disease", {"type": "boolean"}),
+                ("LOINC:LP21258-6", "Oxygen saturation", {"type": "number"}),
+                ("LOINC:30525-0", "Age", {"type": "integer"}),
+                ("LOINC:54134-2", "Race", {"type": "string"}),
+                ("LOINC:54120-1", "Ethnicity", {"type": "string"}),
+                ("LOINC:21840-4", "Sex", {"type": "string"}),
+                ("LOINC:8302-2", "Height", {"type": "number"}),
+                ("LOINC:29463-7", "Weight", {"type": "number"}),
+                ("LOINC:56799-0", "Address", {"type": "string"}),
+                ("LOINC:39156-5", "BMI", {"type": "number"})
+            ]
         ]
-    ]
+    },
+    "pluginTypeTitle": "Mapping"
 }
+
 
 def get_config():
     return config
-
--- test/test_func.py
+++ test/test_func.py
@@ -362,7 +362,6 @@ def query(pid, cv, unit=None, data=None):
         "legalValues": {
             "type": "integer"
         },
-        "how": f"{cv} how",
         "why": f"{cv} why",
         "id": cv,
     }
@@ -1080,38 +1079,41 @@ def test_api_serum_creatinine_from_data_effectiveDateTime_YYYY_MM_DD():
 
 config = {
     "title": "smarthealthit.org variable mapper",
+    'piid': "pdspi-mapper-example",
     "pluginType": "m",
-    "pluginTypeTitle": "Mapping",
-    "pluginSelectors": [],
-    "supportedPatientVariables": [
-        {
-            "id": i,
-            "title": t,
-            "legalValues": lv
-        } for i,t,lv in [
-            ("LOINC:2160-0", "Serum creatinine", {"type": "number"}),
-            ("LOINC:82810-3", "Pregnancy", {"type": "boolean"}),
-            ("HP:0001892", "Bleeding", {"type": "boolean"}),
-            ("HP:0000077", "Kidney dysfunction", {"type": "boolean"}),
-            ("LOINC:45701-0", "Fever", {"type": "boolean"}),
-            ("LOINC:LP212175-6", "Date of fever onset", {"type": "string"}),
-            ("LOINC:64145-6", "Cough", {"type": "boolean"}),
-            ("LOINC:85932-2", "Date of cough onset", {"type": "string"}),
-            ("LOINC:54564-0", "Shortness of breath", {"type": "boolean"}),
-            ("LOINC:LP172921-1", "Cardiovascular disease", {"type": "boolean"}),
-            ("LOINC:54542-6", "Pulmonary disease", {"type": "boolean"}),
-            ("LOINC:LP128504-0", "Autoimmune disease", {"type": "boolean"}),
-            ("LOINC:LP21258-6", "Oxygen saturation", {"type": "number"}),
-            ("LOINC:30525-0", "Age", {"type": "integer"}),
-            ("LOINC:54134-2", "Race", {"type": "string"}),
-            ("LOINC:54120-1", "Ethnicity", {"type": "string"}),
-            ("LOINC:21840-4", "Sex", {"type": "string"}),
-            ("LOINC:8302-2", "Height", {"type": "number"}),
-            ("LOINC:29463-7", "Weight", {"type": "number"}),
-            ("LOINC:56799-0", "Address", {"type": "string"}),
-            ("LOINC:39156-5", "BMI", {"type": "number"})
+    "settingsDefaults": {
+        "pluginSelectors": [],
+        "patientVariables": [
+            {
+                "id": i,
+                "title": t,
+                "legalValues": lv
+            } for i,t,lv in [
+                ("LOINC:2160-0", "Serum creatinine", {"type": "number"}),
+                ("LOINC:82810-3", "Pregnancy", {"type": "boolean"}),
+                ("HP:0001892", "Bleeding", {"type": "boolean"}),
+                ("HP:0000077", "Kidney dysfunction", {"type": "boolean"}),
+                ("LOINC:45701-0", "Fever", {"type": "boolean"}),
+                ("LOINC:LP212175-6", "Date of fever onset", {"type": "string"}),
+                ("LOINC:64145-6", "Cough", {"type": "boolean"}),
+                ("LOINC:85932-2", "Date of cough onset", {"type": "string"}),
+                ("LOINC:54564-0", "Shortness of breath", {"type": "boolean"}),
+                ("LOINC:LP172921-1", "Cardiovascular disease", {"type": "boolean"}),
+                ("LOINC:54542-6", "Pulmonary disease", {"type": "boolean"}),
+                ("LOINC:LP128504-0", "Autoimmune disease", {"type": "boolean"}),
+                ("LOINC:LP21258-6", "Oxygen saturation", {"type": "number"}),
+                ("LOINC:30525-0", "Age", {"type": "integer"}),
+                ("LOINC:54134-2", "Race", {"type": "string"}),
+                ("LOINC:54120-1", "Ethnicity", {"type": "string"}),
+                ("LOINC:21840-4", "Sex", {"type": "string"}),
+                ("LOINC:8302-2", "Height", {"type": "number"}),
+                ("LOINC:29463-7", "Weight", {"type": "number"}),
+                ("LOINC:56799-0", "Address", {"type": "string"}),
+                ("LOINC:39156-5", "BMI", {"type": "number"})
+            ]
         ]
-    ]
+    },
+    "pluginTypeTitle": "Mapping"
 }
 
 
